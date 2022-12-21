c38c81d9b219f7f32eea469b5b8bb8f495b66186
evaaldo <evaaldormj@gmail.com>
2022-12-21 16:32:13 -0300

    fix: correção dos tamanhos de fontes e do botão de dark mode

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 const btn = document.getElementById('btnChangeTheme')
 
 
+function changeColor() {
+
+    if(document.body.style.backgroundColor === 'var(--cinza-fundo)') {
+        document.body.style.backgroundColor = 'var(--preto-fundo)'       
+    } else {
+        document.body.style.backgroundColor = 'var(--cinza-fundo)'
+    }
+
+
+}
+
+btn.addEventListener('click', function() {
+
+    changeColor()
+
+})
